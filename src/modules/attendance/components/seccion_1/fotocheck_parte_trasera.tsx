9764173233c183bbfb3aa27ce2bc40de241fa656
gevodev143-ship-it <gevodev143@gmail.com>
2026-08-17 16:26:59 -0500

    551estoy avanzando con la parte trasera del fotosheck

--- src/modules/attendance/components/seccion_1/fotocheck_parte_trasera.tsx
+++ src/modules/attendance/components/seccion_1/fotocheck_parte_trasera.tsx
@@ -18,7 +18,23 @@ const FotocheckTrasera = forwardRef<HTMLDivElement, FotocheckTraseraProps>(
             className={style.logoHeader}
           />
         </div>
+        <br />
+        
         <div>
+          <hr />
+          <div>
+            <p><b>Misión</b></p>
+            <p>Brindar productos de ferreteria de calidad y soluciones practicas ,con atencion cercana y confiable a nuestros clientes.</p>
+          </div>
+          <hr />
+          <div>
+            <p><b>Visión</b></p>
+            <p>Ser la ferreteria de referencia en la region ,destacando por la calidas ,servicio y soluciones innovadoras.</p>
+          </div>
+          <div>
+
+          </div>
+          <hr />
           <p className={style.telefono}>DNI{dni}</p>
           <p className={style.telefono}>TLF{telefono}</p>
         </div>
